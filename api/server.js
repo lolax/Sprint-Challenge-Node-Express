fe4c984310ff76de8853831b3397f1189e2cfb24
lola <lev@lolas-air.lan>
2018-11-09 12:59:58 -0500

    CRUD + getProjectActions(): projects complete

--- api/server.js
+++ api/server.js
@@ -12,14 +12,14 @@ server.use(cors())
 server.get('/api/actions', (req, res) => {
     actionDb.get()
         .then(actions => res.status(200).json(actions))
-        .catch(err => res.status(500).json({ error: "Couldn't get actions.", err }))
+        .catch(err => res.status(500).json({ error: "The actions could not be retrieved.", err }))
 })
 
 server.get('/api/actions/:id', (req, res) => {
     const { id } = req.params
     actionDb.get(id)
         .then(action => res.status(200).json(action))
-        .catch(err => res.status(500).json({ error: "Couldn't get action.", err }))
+        .catch(err => res.status(500).json({ error: "The action could not be retrieved.", err }))
 })
 
 server.post('/api/actions', (req, res) => {
@@ -27,7 +27,7 @@ server.post('/api/actions', (req, res) => {
     if (project_id && description && notes) {
         actionDb.insert(req.body)
             .then(action => res.status(201).json(action))
-            .catch(err => res.status(500).json({ error: "Couldn't add action.", err }))
+            .catch(err => res.status(500).json({ error: "The action could not be added.", err }))
     } else {
         res.status(400).json({ error: "Please fill out required fields." })
     }
@@ -43,7 +43,7 @@ server.put('/api/actions/:id', (req, res) => {
                 res.status(200).json(action) :
                 res.status(404).json({ error: "Invalid Id." })
             })
-            .catch(err => res.status(500).json({ error: "Couldn't update action.", err }))
+            .catch(err => res.status(500).json({ error: "The action could not be updated.", err }))
     } else {
         res.status(400).json({ error: "Please fill out required fields." })
     }
@@ -57,7 +57,72 @@ server.delete('/api/actions/:id', (req, res) => {
             res.status(204).json() :
             res.status(404).json({ error: "Invalid Id." })
         })
-        .catch(err => res.status(500).json({ error: "Couldn't delete action.", err }))
+        .catch(err => res.status(500).json({ error: "The action could not be deleted.", err }))
+})
+
+// Projects
+server.get('/api/projects', (req, res) => {
+    projectDb.get()
+        .then(projects => res.status(200).json(projects))
+        .catch(err => res.status(500).json({ error: "The projects could not be retrieved.", err }))
+})
+
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params
+    projectDb.get(id)
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({ error: "The project could not be retrieved.", err }))
+})
+
+server.post('/api/projects', (req, res) => {
+    const { name, description } = req.body
+    if (name && description) {
+        projectDb.insert(req.body)
+            .then(project => res.status(201).json(project))
+            .catch(err => res.status(500).json({ error: "The project could not be added.", err }))
+    } else {
+        res.status(400).json({ error: "Please fill out required fields." })
+    }
+})
+
+server.put('/api/projects/:id', (req, res) => {
+    const { id } = req.params
+    const { name, description } = req.body
+    if (name && description) {
+        projectDb.update(id, req.body)
+            .then(project => {
+                project ?
+                res.status(200).json(project) :
+                res.status(404).json({ error: "Invalid Id." })
+            })
+            .catch(err => res.status(500).json({ error: "The project could not be updated.", err }))
+    } else {
+        res.status(400).json({ error: "Please fill out required fields." })
+    }
+})
+
+server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params
+    projectDb.remove(id)
+        .then(count => {
+            count === 1 ?
+            res.status(204).json() :
+            res.status(404).json({ error: "Invalid Id." })
+        })
+        .catch(err => res.status(500).json({ error: "The project could not be deleted.", err }))
+})
+
+server.get('/api/projects/actions/:id', (req, res) => {
+    const { id } = req.params;
+    projectDb.get(id)
+        .then(project => {
+            projectDb.getProjectActions(id)
+            .then(list => res.status(200).json(list))
+            .catch(err => res.status(500).json({ error: "The actions for the specified project could not be retrieved.", err }))
+        })
+        .catch(err => {
+            res.status(404).json({ error: "The project could not be found.", err })
+        })
 })
 
 module.exports = server;
